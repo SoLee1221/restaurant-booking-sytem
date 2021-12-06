ee2c9f4d15c09fc079713fe7b1d3ea435652deee
SoLee1221 <siqbal1221@icloud.com>
2021-12-06 18:27:09 +0000

    create cloudinary links and media links within settings.py and folders

--- booking/models.py
+++ booking/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import user
 
 # Create your models here.
 class Booking(models.Model):
     tables = models.IntegerField(default=1)
     booking_dates = models.DateTimeField()
-    customer = models.Foreignkey(User, on_delete=models.CASCADE, related_name="booking")
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking")
     comment = models.TextField()
     number_of_customers = models.IntegerField(default=1)
 
