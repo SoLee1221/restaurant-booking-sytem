e5e807745719dd413e407894272d26b94014f03b
SoLee1221 <siqbal1221@icloud.com>
2021-12-07 19:40:23 +0000

    create models for booking system, booking and comment

--- booking/models.py
+++ booking/models.py
@@ -1,11 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import user
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Booking(models.Model):
     tables = models.IntegerField(default=1)
     booking_dates = models.DateTimeField()
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking")
-    comment = models.TextField()
     number_of_customers = models.IntegerField(default=1)
-    
+
+class Table_availability(models.Model):
+    t_availability = models.BooleanField(default=True)
+
+class Comment_section(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"comment {self.body} by {self.name}"
