3f7d022ef27a0e7fb79fc55ed45a90423d67ba86
SoLee1221 <siqbal1221@icloud.com>
2021-12-09 18:37:09 +0000

    create admin for body for summernote

--- booking/models.py
+++ booking/models.py
@@ -8,15 +8,15 @@ class Booking(models.Model):
     booking_dates = models.DateTimeField()
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking")
     number_of_customers = models.IntegerField(default=1)
-
-class Table_availability(models.Model):
-    t_availability = models.BooleanField(default=True)
-
-class Comment_section(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    body = models.TextField()
+    body = models.TextField(default='')
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"comment {self.body} by {self.name}"
+
+
+class Table_availability(models.Model):
+    t_availability = models.BooleanField(default=True)
+
