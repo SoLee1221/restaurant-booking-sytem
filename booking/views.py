f1bcd5e1d53ee3b1660cac1aec56eecca23f86ed
SoLee1221 <siqbal1221@icloud.com>
2021-12-14 19:41:10 +0000

    create templates, create views for homepage and tested bookings

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Booking
 
 # Create your views here.
+class PostList(generic.ListView):
+    model = Booking
+    queryset = Booking.objects.order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
